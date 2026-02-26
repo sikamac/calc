18c3b08bfc001a61b9fd032db22a99e76717e714
ksiman <ksiman@dta-sa.com>
2026-02-26 14:57:51 +0000

    fix: update site URL in config and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://gistpoint.com',
+  site: 'https://calculadoraimportacion.com.ar',
   integrations: [
     react(),
     tailwind({
